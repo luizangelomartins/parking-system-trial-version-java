bca0e0210632b35ef97a990e100e66f2904e1f76
Luiz Ângelo Martins <Luiz ngelo Martins@LuizAngelo>
2020-12-05 19:52:42 -0200

    Commit feito para registrar o início do projeto:
    	- Criação do projeto.

---
+++ src/parkingsystem/ParkingSystem.java
@@ -0,0 +1,27 @@
+
+
+
+    // [ "CRIADOR: LUIZ ÂNGELO MARTINS" ]:
+
+
+
+// PACOTE DA CLASSE PRINCIPAL:
+package parkingsystem;
+
+
+public class ParkingSystem {
+
+    
+    // MÉTODO PRINCIPAL DO PROGRAMA - MAIN:
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+    
+}
+
+
+
+
